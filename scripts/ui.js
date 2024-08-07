42e8fff69bc335f1b752b6fb6d717295f033d326
eggrole <bepuetv123@gmail.com>
2024-08-07 23:03:51 +0900

    changeUi and add animation base

---
+++ scripts/ui.js
@@ -0,0 +1,35 @@
+//act ui animation or some
+
+
+
+document.addEventListener('DOMContentLoaded', () => { //요소가 로딩 덜 될수 있어서 하는 것
+
+    const btn_saveTabs = document.getElementById('btn_saveTabs')
+
+    const div_saveTabs = document.getElementById('div_saveTabs')
+
+
+
+    btn_saveTabs.addEventListener('mouseover', () => {
+
+        btn_saveTabs.style.animation = "concealMotherBtn 0.5s forwards"
+
+        
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
