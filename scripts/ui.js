e81188f6ecfb8b20a0583201a12fee2ff369f9f1
eggrole <bepuetv123@gmail.com>
2024-08-18 03:16:27 +0900

    check chorme api

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,16 +1,38 @@
-//act ui animation or some
 
+
+
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions, function (tabs){
+        alert(tabs[0]);
+    });
+
+    return tab;
+};
 
 
 document.addEventListener('DOMContentLoaded', () => { //요소가 로딩 덜 될수 있어서 하는 것
 
-    const btn_saveTabs = document.getElementById('btn_saveTabs')
+    const btn_saveTabs = document.getElementById('btn_saveTabs');
 
     
 
 
 
-    const div_saveTabs = document.getElementById('div_saveTabs')
+    const div_saveTabs = document.getElementById('div_saveTabs');
+
+
+    btn_saveTabs.addEventListener('mousedown', () => {
+
+        chrome.tabs.query({}, (tabs) =>{
+
+            alert(tabs[0].title);
+        });
+
+        
+    });
+
 
 
 
@@ -30,3 +52,4 @@ document.addEventListener('DOMContentLoaded', () => { //요소가 로딩 덜 될
 
 
 });
+
